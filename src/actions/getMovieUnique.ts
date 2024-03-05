5c68e3b97786ba65c14d9ae3aea811761c50688d
Juan Herrera Vázquez <jherrerav1901@alumno.ipn.mx>
2024-03-04 21:00:49 -0600

    fix toSorted a sort

--- src/actions/getMovieUnique.ts
+++ src/actions/getMovieUnique.ts
@@ -7,7 +7,7 @@ export const getMoviesPopularityUnique = async (page: number = 1) => {
         `https://api.themoviedb.org/3/trending/movie/day?api_key=${process.env.IMDB_API_KEY}&language=es-MX&page=${page}&include_adult=false`
     );
     const data: DataMovies = await response.json();
-    const moviesSort = data.results.toSorted((a, b) =>
+    const moviesSort = data.results.sort((a, b) =>
         a.popularity < b.popularity ? 1 : -1
     );
     return moviesSort[0];
